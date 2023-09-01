53ebe8fd43b12e0b3e69a62c9d1fc78c21892fae
Nick Diego <nickmdiego@gmail.com>
2023-09-01 11:17:40 -0500

    Initial release.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd28861602e8bea71b945');
---
+++ src/save.js
@@ -0,0 +1,25 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+export default function Save( props ) {
+	const { attributes } = props;
+	const { showStartingYear, startingYear } = attributes;
+
+	const currentYear = new Date().getFullYear();
+	let displayDate;
+
+	if ( showStartingYear && startingYear ) {
+		displayDate = startingYear + '–' + currentYear;
+	} else {
+		displayDate = currentYear;
+	}
+
+    return (
+		<p { ...useBlockProps.save() }>
+			© { displayDate }
+		</p>
+	);
+}
