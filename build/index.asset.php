6c717ca8af1772cc2c26dbf09a162301086c78ba
Nick Diego <nickmdiego@gmail.com>
2023-11-04 15:58:31 -0500

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3cf7852d632e0e0ac949');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '11106c169183cb78c42e');
--- build/render.php
+++ build/render.php
@@ -1,12 +1,27 @@
 <?php
-$current_year       = date( "Y" );
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+// Get the current year.
+$current_year = date( "Y" );
+
+// Get the block's attribute values, if they exist.
 $starting_year      = isset( $attributes['startingYear'] ) 
-	? $attributes['showStartingYear'] 
+	? $attributes['startingYear'] 
 	: false;
 $show_starting_year = isset( $attributes['showStartingYear'] ) 
 	? $attributes['showStartingYear'] 
 	: false;
 
+// Set the display date.	
 if ( $starting_year && $show_starting_year ) {
 	$display_date = $starting_year . '–' . $current_year;
 } else {
