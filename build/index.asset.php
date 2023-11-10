305e97008449936993b755a6fbad50c367ed49b1
Nick Diego <nickmdiego@gmail.com>
2023-11-10 08:22:03 -0600

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5fec10546346ea1924ef');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c1dda7b53242091d7e4e');
